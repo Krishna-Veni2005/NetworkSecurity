1eedfc0f25f87069537da94c089d2aaa01e41c4d
Krishna <krishnavenisrirangam05@gmail.com>
2026-05-11 22:04:41 +0530

    Data validation completed

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation
 import sys
 if __name__=="__main__":
     try:
         logging.info("Enter thr try bllock")
         trainingpipelineconfig=TrainingPipelineConfig()
-        dataingrstionconfig=DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion=DataIngestion(data_ingestion_config=dataingrstionconfig)
+        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion=DataIngestion(data_ingestion_config=dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data ingestion is completed and artifact is created")
         print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(data_ingestion_artifact=dataingestionartifact,data_validation_config=data_validation_config)
+        logging.info("Initiate the data validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("Data validation is completed and artifact is created")
+        print(data_validation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
